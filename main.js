7b8eac5f87f07d750e8e5efd2b489b389db9d2f3
lina <emaqua323@gmail.com>
2023-01-12 01:35:24 +0900

    Edit button color

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const toggleBtn = document.querySelector('.navbar_tooglebtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
 
-toggleBtn.addEventListener('click', ()=> {
+    toggleBtn.addEventListener('click', ()=> {
     menu.classList.toggle('active'); 
     icons.classList.toggle('active'); 
 });
@@ -23,51 +23,19 @@ $(window).scroll(function() {
     }, 500);
 }).scroll();
 
-function changeBtnName1()  {
-    const btnElement = document.getElementById('btn1');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName2()  {
-    const btnElement = document.getElementById('btn2');
-    btnElement.innerText = 'DOWN';
-}
-function changeBtnName3()  {
-    const btnElement = document.getElementById('btn3');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName4()  {
-    const btnElement = document.getElementById('btn4');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName5()  {
-    const btnElement = document.getElementById('btn5');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName6()  {
-    const btnElement = document.getElementById('btn6');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName7()  {
-    const btnElement = document.getElementById('btn7');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName8()  {
-    const btnElement = document.getElementById('btn8');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName9()  {
-    const btnElement = document.getElementById('btn9');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName10()  {
-    const btnElement = document.getElementById('btn10');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName11()  {
-    const btnElement = document.getElementById('btn11');
-    btnElement.innerText = 'UP';
-}
-function changeBtnName12()  {
-    const btnElement = document.getElementById('btn12');
-    btnElement.innerText = 'DOWN';
-}
+// 버튼 클릭시 배경색상 변경
+var button = document.getElementById("btn1");
+    button.addEventListener("click", function() {
+    this.style.backgroundColor = "red";
+});
+
+// 버튼 클릭시 텍스트 변경
+var button = document.getElementById("btn1");
+    button.addEventListener("click", function() {
+    this.innerHTML = "DOWN";
+});
+
+// 버튼 클릭시 텍스트 변경 기존(20230112이전)
+// function changeBtnName1()  {
+//     const btnElement = document.getElementById('btn1');
+//     btnElement.innerText = 'UP';
